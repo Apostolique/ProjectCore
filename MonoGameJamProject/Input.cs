b1f10218f699fc8f1213e17a3742e8b7afd4db49
Tim "Lightning2x" Schagen <schagen34@gmail.com>
2018-02-12 00:32:37 +0100

    Added a simple input for mouse

---
+++ MonoGameJamProject/Input.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameJamProject
+{
+    /// <summary>
+    /// Basic class for handling Input
+    /// </summary>
+    class Input
+    {
+        protected MouseState currentMouseState, previousMouseState;
+
+        public void Update()
+        {
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+        }
+
+        public Vector2 MousePosition
+        {
+            get { return new Vector2(currentMouseState.X, currentMouseState.Y); }
+        }
+
+        public bool MouseLeftButtonPressed()
+        {
+            return currentMouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released;
+        }
+
+        public bool MouseRightButtonPressed()
+        {
+            return currentMouseState.RightButton == ButtonState.Pressed && previousMouseState.RightButton == ButtonState.Released;
+        }
+    }
+}
