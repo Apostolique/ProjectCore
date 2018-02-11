f59a756961c3df39676a561d79ddf4c3a9997e5a
Apostolique <jeandavidmoisan@gmail.com>
2018-02-11 17:53:18 -0500

    New path class

---
+++ MonoGameJamProject/Path.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace MonoGameJamProject
+{
+    /// <summary>
+    /// Goal: Base class for the game's minions.
+    /// </summary>
+    class Path
+    {
+        public Path() {
+        }
+
+        public void Draw(SpriteBatch s, int gridSize) {
+        }
+    }
+}
