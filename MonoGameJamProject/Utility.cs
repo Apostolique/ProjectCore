75227e7b6d01e64d0b88141ade354e3479226e17
Apostolique <jeandavidmoisan@gmail.com>
2018-02-12 08:20:35 -0500

    comments were wrong

--- MonoGameJamProject/Utility.cs
+++ MonoGameJamProject/Utility.cs
@@ -24,13 +24,13 @@ namespace MonoGameJamProject
             return value * gridSize - gridSize;
         }
         /// <summary>
-        /// Converts a game coordinate into a screen coordinate.
+        /// Converts a screen coordinate into a game coordinate.
         /// </summary>
         public static int ScreenToGame(int value, int gridSize) {
             return (value + gridSize) / gridSize;
         }
         /// <summary>
-        /// Converts a game coordinate into a screen coordinate.
+        /// Converts a screen coordinate into a game coordinate.
         /// </summary>
         public static float ScreenToGame(float value, int gridSize) {
             return (value + gridSize) / gridSize;
