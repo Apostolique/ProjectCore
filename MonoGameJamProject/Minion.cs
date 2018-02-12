d6b677d552318f2436ddc97cdef1dbba58082119
Apostolique <jeandavidmoisan@gmail.com>
2018-02-12 13:48:12 -0500

    Started working on the minion movements

--- MonoGameJamProject/Minion.cs
+++ MonoGameJamProject/Minion.cs
@@ -12,18 +12,41 @@ namespace MonoGameJamProject
     /// </summary>
     class Minion
     {
-        float x;
-        float y;
+        public Vector2 position
+        {
+            get; set;
+        }
+        float speed;
+
+        Vector2 target;
+        float reach;
+        float reachSpeed;
+
         public Minion(float iX, float iY)
         {
-            x = iX;
-            y = iY;
+            position = new Vector2(iX, iY);
+            speed = 1;
+            reach = 1;
+        }
+        public bool IsMoving => reach < 1;
+        public void Update()
+        {
+            if (IsMoving) {
+                position = Vector2.Lerp(position, target, reach);
+            }
+        }
+        public void MoveTo(Vector2 b)
+        {
+            reach = 0;
+            target = b;
+            float distance = Vector2.Distance(position, target);
+            reachSpeed = distance / speed;
         }
 
         public void Draw(SpriteBatch s, float size, int gridSize)
         {
             float halfSize = size * gridSize / 2f;
-            s.FillRectangle(new RectangleF(Utility.GameToScreen(x, gridSize) - halfSize, Utility.GameToScreen(y, gridSize) - halfSize, size * gridSize, size * gridSize), Color.Green);
+            s.FillRectangle(new RectangleF(Utility.GameToScreen(position.X, gridSize) - halfSize, Utility.GameToScreen(position.Y, gridSize) - halfSize, size * gridSize, size * gridSize), Color.Green);
         }
     }
 }
