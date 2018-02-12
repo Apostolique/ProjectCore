fb629c8f07b92b7a2626ddd3fe274aefbfa7313d
Apostolique <jeandavidmoisan@gmail.com>
2018-02-12 15:49:38 -0500

    Minions can follow waypoints!

--- MonoGameJamProject/Minion.cs
+++ MonoGameJamProject/Minion.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -16,11 +17,22 @@ namespace MonoGameJamProject
         {
             get; set;
         }
+        struct waypoint
+        {
+            public Vector2 start;
+            public Vector2 target;
+            public float distance;
+            
+            public waypoint(Vector2 iStart, Vector2 iTarget, float iDistance)
+            {
+                start = iStart;
+                target = iTarget;
+                distance = iDistance;
+            }
+        }
+        List<waypoint> waypoints;
         float speed;
 
-        Vector2 start;
-        Vector2 target;
-        float distance;
         float inBetween;
         public bool dead;
         int hp;
@@ -30,24 +42,45 @@ namespace MonoGameJamProject
             //FIXME: The speed and movement is really wrong.
             position = new Vector2(iX, iY);
             speed = 0.005f;
-            distance = 0;
             inBetween = 0;
+
+            waypoints = new List<waypoint>();
         }
-        public bool IsMoving => inBetween < distance;
+        public bool IsMoving
+        {
+            get
+            {
+                if (waypoints.Count > 0) {
+                    return inBetween < waypoints[0].distance;
+                }
+                return false;
+            }
+        }
         public void Update(GameTime gameTime)
         {
-            if (IsMoving) {
+            while (waypoints.Count() > 0 && inBetween >= waypoints.First().distance) {
+                inBetween -= waypoints[0].distance;
+                waypoints.RemoveAt(0);
+            }
+            if (waypoints.Count > 0) {
                 inBetween += speed * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-                inBetween = Math.Min(distance, inBetween);
-                position = Vector2.Lerp(start, target, inBetween / distance);
+                inBetween = Math.Min(waypoints[0].distance, inBetween);
+                position = Vector2.Lerp(waypoints[0].start, waypoints[0].target, inBetween / waypoints[0].distance);
+            } else {
+                inBetween = 0;
             }
         }
         public void MoveTo(Vector2 b)
         {
-            inBetween = 0;
-            start = position;
-            target = b;
-            distance = Vector2.Distance(start, target);
+            if (waypoints.Count > 0) {
+                waypoint waypoint = new waypoint(waypoints.Last().target, b, Vector2.Distance(waypoints.Last().target, b));
+                waypoints.Add(waypoint);
+            } else {
+                waypoint waypoint = new waypoint(position, b, Vector2.Distance(position, b));
+                waypoints.Add(waypoint);
+            }
+
+            //inBetween = 0;
         }
 
         public void Draw(SpriteBatch s, float size, int gridSize)
