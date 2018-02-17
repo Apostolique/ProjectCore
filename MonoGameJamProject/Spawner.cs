bbe83edb576515f930b175f2032cc8cd14526076
Apostolique <jeandavidmoisan@gmail.com>
2018-02-17 12:27:29 -0500

    Added a new Spawner class

---
+++ MonoGameJamProject/Spawner.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameJamProject.Towers;
+using MonoGameJamProject.UI;
+using MonoGame.Extended;
+
+namespace MonoGameJamProject
+{
+    /// <summary>
+    /// Goal: A class in charge of spawning minions.
+    /// </summary>
+    class Spawner
+    {
+        CoolDownTimer cd;
+        public Spawner()
+        {
+        }
+    }
+}
+
